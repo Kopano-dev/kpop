31084e3712c66d1108505d4ece78a7bcf24bfae7
Simon Eisenmann <s.eisenmann@kopano.com>
2018-07-18 12:06:26 +0200

    Add retry support to loader
    Whe the loader has errored or stuck it is essential to guide the user to
    a way out. This change adds a retry action to the loaders error and
    timedOut states which will reload the page when clicked.

--- src/Loading/Loading.js
+++ src/Loading/Loading.js
@@ -3,16 +3,22 @@ import PropTypes from 'prop-types';
 
 import {FormattedMessage} from 'react-intl';
 
+function handleRetry(event) {
+  event.preventDefault();
+
+  window.location.reload();
+}
+
 function Loading(props) {
   if (props.error) {
     // When the loader has errored.
     return <div id="loader">
-      <FormattedMessage id="kpop.loader.error.message" defaultMessage="Error!"></FormattedMessage>
+      <FormattedMessage id="kpop.loader.error.message" defaultMessage="Error!"></FormattedMessage> &mdash; <a href="#" onClick={ handleRetry }><FormattedMessage id="kpop.loader.retry" defaultMessage="retry"></FormattedMessage></a>
     </div>;
   } else if (props.timedOut) {
     // When the loader has taken longer than the timeout.
     return <div id="loader">
-      <FormattedMessage id="kpop.loader.longtime.message" defaultMessage="Taking a long time..."></FormattedMessage>
+      <FormattedMessage id="kpop.loader.longtime.message" defaultMessage="Taking a long time..."></FormattedMessage> &mdash; <a href="#" onClick={ handleRetry }><FormattedMessage id="kpop.loader.retry" defaultMessage="retry"></FormattedMessage></a>
     </div>;
   } else if (props.pastDelay) {
     // When the loader has taken longer than the delay.
