95e9109a8e79bf62098b7d1069dd9f019ec66c8e
Jelle van der Waa <j.vanderwaa@kopano.com>
2018-08-07 15:39:59 +0200

    Add url encoding functions from meet to kpop
    Introduce forceBase64URLEncoded and forceBase64StdEncoded.

--- src/utils/index.js
+++ src/utils/index.js
@@ -44,3 +44,15 @@ export function encodeParams(data) {
   }
   return ret.join('&');
 }
+
+export function forceBase64URLEncoded(s) {
+  // Converts Base64 Standard encoded string to Base64 URL encoded string. See
+  // https://tools.ietf.org/html/rfc4648#section-5 for the specification.
+  return s.replace(/\+/g, '-').replace(/\//, '_');
+}
+
+export function forceBase64StdEncoded(s) {
+  // Converts Base64 URL encoded string to Base64 Standard encoded string. See
+  // https://tools.ietf.org/html/rfc4648#section-5 for the specification.
+  return s.replace(/-/g, '+').replace(/_/, '/');
+}
