c08e8cf28c2dcf27311e9487adbbf0d59e3a836a
Simon Eisenmann <s.eisenmann@kopano.com>
2018-09-05 12:01:33 +0200

    Add promise based debounce with cancel support

---
+++ src/utils/debounce.js
@@ -0,0 +1,32 @@
+/*
+ * debounce offers  promise based debouncer function with cancel support.
+ */
+export function debounce(callable, delay=250) {
+  let t = null;
+  let resolvers = [];
+  let cancelled = false;
+
+  const cancel = () => {
+    cancelled = true;
+    clearTimeout(t);
+  };
+
+  return (...args) => {
+    t = setTimeout(() => {
+      if (cancelled) {
+        return;
+      }
+      let r = callable(...args);
+      resolvers.forEach(resolver => resolver(r));
+      resolvers = [];
+    }, delay);
+
+    // Create new promise with cancel support.
+    const p = new Promise(resolver => resolvers.push(resolver));
+    p.cancel = cancel;
+
+    return p;
+  };
+}
+
+export default debounce;
