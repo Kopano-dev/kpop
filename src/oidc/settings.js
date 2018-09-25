f414cd8a7b588ce0e3725b49e4f1bbd8ef4ecbbd
Simon Eisenmann <s.eisenmann@kopano.com>
2018-09-25 12:13:25 +0200

    oidc: Use fragment URL for silent refresh
    To avoid additional web server configuration and to simplify Konnect
    configuration this change moves the silent refresh url to a hash based
    URL fragment identifier similar to the other internal oidc routes.

--- src/oidc/settings.js
+++ src/oidc/settings.js
@@ -1,6 +1,6 @@
 export const AUTHORIZE_CALLBACK_MARKER = '#oidc-callback';
 export const ENDSESSION_CALLBACK_MARKER = '#oidc-endsession-callback';
-export const SILENT_REFRESH_MARKER = '/oidc-silent-refresh.html';
+export const SILENT_REFRESH_MARKER = '#oidc-silent-refresh';
 
 export const settings = {
 };
@@ -9,7 +9,7 @@ export async function setup(appBaseURL=window.location.href) {
   settings.appBaseURL = appBaseURL;
   settings.redirectURL = `${appBaseURL}${AUTHORIZE_CALLBACK_MARKER}`;
   settings.postLogoutRedirectURL = `${appBaseURL}${ENDSESSION_CALLBACK_MARKER}`;
-  settings.silentRedirectURL = appBaseURL.replace(/\/$/, '') + SILENT_REFRESH_MARKER;
+  settings.silentRedirectURL = `${appBaseURL}${SILENT_REFRESH_MARKER}`;
 
   console.info('oidc setup complete', settings); // eslint-disable-line no-console
 }
