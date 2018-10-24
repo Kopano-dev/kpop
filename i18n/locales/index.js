3675136f1d74eeae4bf5efb3c5edc7366cdfe473
Simon Eisenmann <s.eisenmann@kopano.com>
2018-10-24 11:47:04 +0200

    Add and update languages for translation

--- i18n/locales/index.js
+++ i18n/locales/index.js
@@ -1,5 +1,27 @@
+// NOTE(longsleep): This loads all translation files to be included in the
+// app bundle. They are not that large.
+
+// Please keep imports and exports alphabetically sorted.
 import de from './de.json';
+import es from './es.json';
+import fr from './fr.json';
+import hu from './hu.json';
+import it from './it.json';
+import ja from './ja.json';
+import nb from './nb.json';
+import nl from './nl.json';
+import ptPT from './pt_PT.json';
+import ru from './ru.json';
 
 export default {
   de,
+  es,
+  fr,
+  hu,
+  it,
+  ja,
+  nb,
+  nl,
+  ptPT,
+  ru,
 };
