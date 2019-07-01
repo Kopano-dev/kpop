62569414a8a1723d67c1c541dd1ad04c15bf3663
Simon Eisenmann <s.eisenmann@kopano.com>
2019-07-01 12:33:06 +0200

    Provide common reducer and selectors

---
+++ src/common/selectors.js
@@ -0,0 +1,18 @@
+/* Private selectors.
+ *
+ * These selectors follow the idea that they get passed in only the part of
+ * the store which is needed by them by the application actually using the
+ * selector and has control of the store. Do not pass the whole store, so that
+ * the apps stay in control of the nesting and data structure.
+ *
+ */
+
+import { scopeGuestOK } from '../oidc/scopes';
+
+export function hasScope(store, requiredScope) {
+  return store.user && store.user.scope.indexOf(requiredScope);
+}
+
+export function isGuest(store) {
+  return hasScope(store, scopeGuestOK);
+}
