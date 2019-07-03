536e6837b4499679798a8a53f3a9a204d5c879d7
Simon Eisenmann <s.eisenmann@kopano.com>
2019-07-03 12:13:45 +0200

    Fixup error testing for coverage

--- src/common/errors.spec.js
+++ src/common/errors.spec.js
@@ -3,16 +3,7 @@ import {
 } from './errors';
 
 test('unexpectedNetworkResponseError', () => {
-  expect(() => {
-    try {
-      const err = new UnexpectedNetworkResponseError('test error', 418);
-      throw err;
-    } catch(e) {
-      if (e.status === 418) {
-        throw e;
-      } else {
-        throw new Error('failed');
-      }
-    }
-  }).toThrowError(UnexpectedNetworkResponseError);
+  expect((() => {
+    return new UnexpectedNetworkResponseError('test error', 418).status;
+  })()).toEqual(418);
 });
