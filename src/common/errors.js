81be991dfe9474131f645ee8b1279614920406df
Simon Eisenmann <s.eisenmann@kopano.com>
2019-12-13 15:31:36 +0100

    Properly store message with errors

--- src/common/errors.js
+++ src/common/errors.js
@@ -2,7 +2,7 @@ export class UnexpectedNetworkResponseError extends Error {
   handled = false;
 
   constructor(message, status, ...args) {
-    super(...args);
+    super(message, ...args);
     this.status = status;
     if (Error.captureStackTrace !== undefined) {
       Error.captureStackTrace(this, UnexpectedNetworkResponseError);
